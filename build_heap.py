3656b835c6c00d9c62b88f346cf67ae68e9b5f5b
eduards4grupa <125820885+eduards4grupa@users.noreply.github.com>
2023-03-20 01:04:31 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -5,22 +5,41 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    n=len(data)
+    for i in range (n//2,-1,-1):
+        minHeapify(array, size, k)
     return swaps
 
+def minHeapify(array, size, k)
+    n=len(data)
+    set k as smallest
+    leftChild = 2*k + 1
+    rightChild = 2*k + 2
+  
+    if leftChild < n and array[leftChild] < array[smallest]:
+        set leftChildIndex as smallest
+    if rightChild < n and array[rightChild]<array[smallest]:
+        set rightChildIndex as smallest
+        
+  
 
 def main():
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
+    iof = input()
+    
+    if iof = "I":
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
+        n = int(input())
+        data = list(map(int, input().split()))
+    if iof = "F":
+        filename=input()
+        with open ('./tests/'filename,"r") as f:
+            read=int(f.read()) 
+            data = list(map(int, input().split()))
+        
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
